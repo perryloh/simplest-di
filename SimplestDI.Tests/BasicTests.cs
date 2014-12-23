fad8a2d934e77d480fd733b3f3472b980af13ae3
unknown <LohP@MININT-F4MBHJA.Nintex.com>
2014-12-23 13:10:42 +0800

    'First'

---
+++ SimplestDI.Tests/BasicTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimplestDI;
+
+namespace SimplestDI.Tests
+{   
+    public class TestStub {
+
+    }
+
+    public class TestStubDependency
+    {
+        public TestStub Stub {get;set;}
+
+        public TestStubDependency(TestStub stub)
+        {
+            Stub = stub;
+        }
+    }
+
+    [TestClass]
+    public class BasicTests
+    {
+        [TestMethod]
+        public void Register_Test()
+        {
+            var container = new SimplestContainer();
+            container.Register<TestStub>();
+            
+            var result = container.Create<TestStub>();
+            Assert.IsInstanceOfType(result, typeof(TestStub));
+        }
+
+        [TestMethod]
+        public void Register_Constructor_DI_Test()
+        {
+            var container = new SimplestContainer();
+            container.Register<TestStub>();
+            container.Register<TestStubDependency>();
+
+            var result = container.Create<TestStubDependency>();
+            Assert.IsInstanceOfType(result.Stub, typeof(TestStub));
+
+        }
+
+           
+        
+
+
+    }
+}
---
+++ SimplestDI/SimplestContainer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplestDI
+{
+    public class SimplestContainer
+    {
+        private SortedList<string, Type> container = new SortedList<string, Type>();
+
+        public void Register<T>()
+        {
+            container.Add(typeof(T).FullName, typeof(T));
+        }
+
+        public T Create<T>()
+        {
+            return GetInstance<T>(container[typeof(T).FullName]);
+        }
+
+        private T GetInstance<T>(Type t)
+        {
+            // check for dependencies in constructor methods
+            ConstructorInfo[] ci = t.GetConstructors();
+            foreach (ConstructorInfo info in ci)
+            {
+                // look thru the parameters of each constructor
+                ParameterInfo[] pi = info.GetParameters();
+                foreach (ParameterInfo p in pi)
+                {
+                    // check if a constructor parameter is of non-primitive type to rule them out
+                    var paramType = p.ParameterType;
+                    if (!paramType.IsPrimitive)
+                    {
+                        // found possible dependency - check against container 
+                        // TODO - only supports one parameter/dependency for now
+                        Type DependsOn = null;                        
+                        if (container.TryGetValue(paramType.FullName, out DependsOn)) {
+                            // yes its a dependency we are managing so lets instantiate that
+                            var DependsOnObj = System.Activator.CreateInstance(DependsOn);
+
+                            // INJECT that DEPENDENCY in
+                            return (T)System.Activator.CreateInstance(t, DependsOnObj);
+                        }
+                    }
+                    
+                }
+            }
+
+            // no dependencies - the default simplest instance to be returned 
+            return (T) System.Activator.CreateInstance(t);
+        }
+    }
+}
